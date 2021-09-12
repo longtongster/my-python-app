0ac1b40c9019bb0b2286b1ecd23d0ff4de1f11d0
longtongster <sacha.van.weeren@gmail.com>
2021-09-12 11:10:59 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+# Pycharm course:
+# https://www.youtube.com/watch?v=hc50ALh_x5g
+#
+# Keyboard shortcuts:
+# Alt + 1 - open or close the project tab (right side of the screen)
+# Ctrl + / - can be used comment selected lines
+# Ctrl + f5 - rerun
+# Shift + f10 = run
+# CTRL Alt S (settings)
+# under settings you can install pluggins and keymaps
+# under settings - editor - color scheme I have set Monokai
+# CTRL + backtick key - opens a menu with quick access to settingse5
+
+# Continue Pycharm video tutorial at 29:24
+
+if __name__ == '__main__':
+    print("Pycharm is Awesome")
